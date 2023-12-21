06979b18cda2937445612d1fb4335c8944c23987
SankalpDesai <sankalpdesai00@gmail.com>
2023-12-21 13:43:14 +0530

    first commit code deploy

---
+++ src/test/java/CucumberOptions/FailedTestRunner.java
@@ -0,0 +1,19 @@
+package CucumberOptions;
+
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+//html reports, xml reports, json reports, junit reports, extent report
+@CucumberOptions(features="@target/failed_scenarios.txt", glue="stepDefinition",
+ monochrome=true,
+ plugin= {"html:target/cucumber.html", "json:target/cucumber.json",
+		 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+public class FailedTestRunner extends AbstractTestNGCucumberTests{
+
+	@Override
+	@DataProvider(parallel=true)
+	public Object[][] scenarios(){
+		return super.scenarios();
+	}
+}
---
+++ src/test/java/CucumberOptions/TestNGTestRunner.java
@@ -0,0 +1,20 @@
+package CucumberOptions;
+
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+//html reports, xml reports, json reports, junit reports, extent report
+@CucumberOptions(features="src/test/java/features", glue="stepDefinition",
+ monochrome=true,tags="@OfferPage or @PlaceOrder",
+ plugin= {"html:target/cucumber.html", "json:target/cucumber.json",
+		 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+		 "rerun:target/failed_scenarios.txt"})
+public class TestNGTestRunner extends AbstractTestNGCucumberTests{
+
+	@Override
+	@DataProvider(parallel=true)
+	public Object[][] scenarios(){
+		return super.scenarios();
+	}
+}
